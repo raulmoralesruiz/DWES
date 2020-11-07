67b320cdd520940df4d88d4af5aa8388d0a2c7d9
Raúl Morales <raulmdaw@gmail.com>
2020-11-07 21:25:48 +0100

    Movimiento de archivos

---
+++ 1ev/CRUD_rest/basura/entity/old/ComparaClienteNombre.java
@@ -0,0 +1,16 @@
+package z_com.jacaranda.entity.old;
+
+import java.util.Comparator;
+
+import com.jacaranda.entity.Customer;
+
+public class ComparaClienteNombre implements Comparator<Customer> {
+
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		return c1.getName().compareTo(c2.getName());
+	}
+	
+	
+
+}
---
+++ 1ev/CRUD_rest/basura/entity/old/Old_Suscription.java
@@ -0,0 +1,45 @@
+package z_com.jacaranda.entity.old;
+
+import com.jacaranda.entity.SuscriptionEnum;
+
+public class Old_Suscription {
+
+	private int idSuscription;
+	private static int idSiguiente = 0;
+	private SuscriptionEnum tipo;  
+
+	
+	
+	public Old_Suscription() {
+		super();
+	}
+	public Old_Suscription(SuscriptionEnum tipo) {
+		super();
+		this.idSuscription = idSiguiente++;
+		this.tipo = tipo;
+	}
+	
+	
+	
+	public int getIdSuscription() {
+		return idSuscription;
+	}
+	public void setIdSuscription(int idSuscription) {
+		this.idSuscription = idSuscription;
+	}
+	public static int getIdSiguiente() {
+		return idSiguiente;
+	}
+	public static void setIdSiguiente(int idSiguiente) {
+		Old_Suscription.idSiguiente = idSiguiente;
+	}
+	public SuscriptionEnum getTipo() {
+		return tipo;
+	}
+	public void setTipo(SuscriptionEnum tipo) {
+		this.tipo = tipo;
+	}
+	
+
+	
+}
